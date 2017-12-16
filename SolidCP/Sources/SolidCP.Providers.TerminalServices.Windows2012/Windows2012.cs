0d8466d704ca341c88c9bb2c6c6e581fa3c87750
Trevor Robinson <t.robinson@key4ce.com>
2017-12-16 20:40:51 +0000

    RDS2012 changes to support Windows 2016.

--- SolidCP/Sources/SolidCP.Providers.TerminalServices.Windows2012/Windows2012.cs
+++ SolidCP/Sources/SolidCP.Providers.TerminalServices.Windows2012/Windows2012.cs
@@ -179,7 +179,7 @@ namespace SolidCP.Providers.RemoteDesktopServices
         public override bool IsInstalled()
         {            
             Server.Utils.OS.WindowsVersion version = SolidCP.Server.Utils.OS.GetVersion();
-            return version == SolidCP.Server.Utils.OS.WindowsVersion.WindowsServer2012 || version == SolidCP.Server.Utils.OS.WindowsVersion.WindowsServer2012R2;
+            return version == SolidCP.Server.Utils.OS.WindowsVersion.WindowsServer2012 || version == SolidCP.Server.Utils.OS.WindowsVersion.WindowsServer2012R2 || version == SolidCP.Server.Utils.OS.WindowsVersion.WindowsServer2016;
         }
 
         public override string[] Install()
