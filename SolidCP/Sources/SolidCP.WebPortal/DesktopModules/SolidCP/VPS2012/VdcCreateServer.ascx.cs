35e5e5b4cee3d3883055f03b5d278480b90162fd
berkut <genadij.judin@dataclub.lv>
2018-05-08 02:45:02 +0300

    [Portal] Hides the obvious choice of VLAN for creating VPS

--- SolidCP/Sources/SolidCP.WebPortal/DesktopModules/SolidCP/VPS2012/VdcCreateServer.ascx.cs
+++ SolidCP/Sources/SolidCP.WebPortal/DesktopModules/SolidCP/VPS2012/VdcCreateServer.ascx.cs
@@ -67,7 +67,7 @@ namespace SolidCP.Portal.VPS2012
             if (!PackagesHelper.IsQuotaEnabled(PanelSecurity.PackageId, Quotas.VPS2012_EXTERNAL_NETWORK_ENABLED))
             {
                 wizard.WizardSteps.Remove(stepExternalNetwork);
-                //chkExternalNetworkEnabled.Checked = false;
+                chkExternalNetworkEnabled.Checked = false;
             }
 
             //KD FSJ
@@ -180,7 +180,8 @@ namespace SolidCP.Portal.VPS2012
 
                 //List<int> vlans = ES.Services.VPS2012.GetAvailableVLANs(PanelSecurity.PackageId).vlans;
                 listVlanLists.Items.Clear();
-                listVlanLists.Items.Add(new ListItem("External network disabled", "-1"));
+                //listVlanLists.Items.Add(new ListItem("External network disabled", "-1"));
+                listVlanLists.Items.Add(new ListItem("Select a VLAN", "-1"));
                 foreach (int vlan in vlans)
                 {
                     listVlanLists.Items.Add(new ListItem(String.Format("VLAN {0}", vlan.ToString()), vlan.ToString()));
@@ -291,9 +292,21 @@ namespace SolidCP.Portal.VPS2012
             // external network
             bool emptyIps = listExternalAddresses.Items.Count == 0;
             EmptyExternalAddressesMessage.Visible = emptyIps;
-            tableExternalNetwork.Visible = !emptyIps && (Convert.ToInt32(listVlanLists.SelectedValue) >= 0);
-            //chkExternalNetworkEnabled.Enabled = !emptyIps;
-            //chkExternalNetworkEnabled.Checked = chkExternalNetworkEnabled.Checked && !emptyIps;
+            listVlanLists.Visible = !emptyIps && (listVlanLists.Items.Count > 2); //First is fake ("Select Vlan")
+            if (!emptyIps && (listVlanLists.Items.Count > 2))
+            {
+                tableExternalNetwork.Visible = !emptyIps && (Convert.ToInt32(listVlanLists.SelectedValue) >= 0);
+                //TODO: set the first possible VLAN?
+            }
+            else
+            {
+                tableExternalNetwork.Visible = chkExternalNetworkEnabled.Checked && !emptyIps;
+                chkExternalNetworkEnabled.Enabled = !emptyIps;
+                chkExternalNetworkEnabled.Checked = chkExternalNetworkEnabled.Checked && !emptyIps;
+                listVlanLists.SelectedIndex = listVlanLists.Items.Count - 1;
+            }
+
+
             ExternalAddressesNumberRow.Visible = radioExternalRandom.Checked;
             ExternalAddressesListRow.Visible = radioExternalSelected.Checked;
 
@@ -362,10 +375,11 @@ namespace SolidCP.Portal.VPS2012
             optionReinstall.Value = chkReinstall.Checked;
 
             // external network
-            optionExternalNetwork.Value = (Convert.ToInt32(listVlanLists.SelectedValue) >= 0);
-            SummExternalAddressesNumberRow.Visible = radioExternalRandom.Checked && (Convert.ToInt32(listVlanLists.SelectedValue) >= 0);
+            bool isSelectedAndChecked = chkExternalNetworkEnabled.Checked && (Convert.ToInt32(listVlanLists.SelectedValue) >= 0);
+            optionExternalNetwork.Value = isSelectedAndChecked;
+            SummExternalAddressesNumberRow.Visible = radioExternalRandom.Checked && isSelectedAndChecked;
             litExternalAddressesNumber.Text = PortalAntiXSS.Encode(txtExternalAddressesNumber.Text.Trim());
-            SummExternalAddressesListRow.Visible = radioExternalSelected.Checked && (Convert.ToInt32(listVlanLists.SelectedValue) >= 0);
+            SummExternalAddressesListRow.Visible = radioExternalSelected.Checked && isSelectedAndChecked;
 
             List<string> ipAddresses = new List<string>();
             foreach (ListItem li in listExternalAddresses.Items)
