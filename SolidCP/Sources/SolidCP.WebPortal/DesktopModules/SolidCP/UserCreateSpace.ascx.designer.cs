bd6c69f593fa63838e12ba1fa349bad405285e92
berkut <genadij.judin@dataclub.lv>
2018-07-03 00:38:38 +0300

    [Portal] Make the panel more friendly, part 2. Now you can add add-on, when you create Hosting Space and make redirection to create VPS2012/2016 ignore useless steps.

--- SolidCP/Sources/SolidCP.WebPortal/DesktopModules/SolidCP/UserCreateSpace.ascx.designer.cs
+++ SolidCP/Sources/SolidCP.WebPortal/DesktopModules/SolidCP/UserCreateSpace.ascx.designer.cs
@@ -94,6 +94,51 @@ namespace SolidCP.Portal {
         protected global::System.Web.UI.WebControls.DropDownList ddlStatus;
         
         /// <summary>
+        /// btnAddAddon control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddAddon;
+        
+        /// <summary>
+        /// rbPlanQuotas control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbPlanQuotas;
+        
+        /// <summary>
+        /// rbPackageQuotas control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton rbPackageQuotas;
+        
+        /// <summary>
+        /// repHostingAddons control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater repHostingAddons;
+        
+        /// <summary>
+        /// chkRedirectToCreateVPS control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkRedirectToCreateVPS;
+        
+        /// <summary>
         /// chkPackageLetter control.
         /// </summary>
         /// <remarks>
