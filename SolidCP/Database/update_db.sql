f6ad5a9091b20fdc05af46693864c3b160514ee7
Trevor Robinson <t.robinson@key4ce.com>
2019-01-31 21:07:48 +0000

    Fix for lines removed from update_db.sql in last commit

--- SolidCP/Database/update_db.sql
+++ SolidCP/Database/update_db.sql
@@ -1,4 +1,15 @@
+USE [${install.database}]
+GO
+-- update database version
+DECLARE @build_version nvarchar(10), @build_date datetime
+SET @build_version = N'${release.version}'
+SET @build_date = '${release.date}T00:00:00' -- ISO 8601 Format (YYYY-MM-DDTHH:MM:SS)
 
+IF NOT EXISTS (SELECT * FROM [dbo].[Versions] WHERE [DatabaseVersion] = @build_version)
+BEGIN
+	INSERT [dbo].[Versions] ([DatabaseVersion], [BuildDate]) VALUES (@build_version, @build_date)
+END
+GO
 
 -- Fix for Some problems with collate in GetDnsRecordsTotal
 IF EXISTS (SELECT * FROM SYS.OBJECTS WHERE type = 'P' AND name = 'GetDnsRecordsTotal')
