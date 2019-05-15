ef441cdefaa73e99f237dda98de930cd2a807818
Marco Tiggelaar <m.tiggelaar@key4ce.com>
2019-05-15 14:55:24 +0200

    Added VS 2019 build support.

--- SolidCP/deploy-release.bat
+++ SolidCP/deploy-release.bat
@@ -3,7 +3,19 @@ RMDIR /S /Q "Bin"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 
-IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" (
+	Set SCPMSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+	Set SCPVSVer=16.0
+	Echo Found VS 2019 Community
+) ELSE IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" (
+	Set SCPMSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
+	Set SCPVSVer=16.0
+	Echo Found VS 2019 Professional
+) ELSE IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" (
+	Set SCPMSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+	Set SCPVSVer=16.0
+	Echo Found VS 2019 Enterprise
+} ELSE IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
 	Set SCPMSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 	Set SCPVSVer=15.0
 	Echo Found VS 2017 Community
@@ -20,5 +32,7 @@ IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.
 	Set SCPVSVer=14.0
 	Echo Found VS 2015
 )
+
+%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe
 
 %SCPMSBuild% build.xml /target:Deploy /p:BuildConfiguration=Release /p:Version="1.4.3" /p:FileVersion="1.4.3" /p:VersionLabel="1.4.3" /v:n /fileLogger /m /p:VisualStudioVersion=%SCPVSVer%
