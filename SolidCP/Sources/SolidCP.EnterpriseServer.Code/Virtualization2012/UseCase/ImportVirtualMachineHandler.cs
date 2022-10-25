8128a3fcb8cc26c5b6c1ccd0107a2230ed596c54
Ivan Schulz <qqqschulz@gmail.com>
2022-10-25 10:24:29 +0200

    Resolve merge conflict for 7ab03e8

--- SolidCP/Sources/SolidCP.EnterpriseServer.Code/Virtualization2012/UseCase/ImportVirtualMachineHandler.cs
+++ SolidCP/Sources/SolidCP.EnterpriseServer.Code/Virtualization2012/UseCase/ImportVirtualMachineHandler.cs
@@ -150,10 +150,14 @@ namespace SolidCP.EnterpriseServer.Code.Virtualization2012.UseCase
                     {
                         totalHddSize += item.HddSize[i];
                     }
-                    QuotaHelper.CheckNumericQuota(cntx, quotaResults, Quotas.VPS2012_HDD, totalHddSize, VirtualizationErrorCodes.QUOTA_EXCEEDED_HDD);
                     if (item.HddSize.Length > 1)
                     {
+                        QuotaHelper.CheckNumericQuota(cntx, quotaResults, Quotas.VPS2012_HDD, totalHddSize, VirtualizationErrorCodes.QUOTA_EXCEEDED_HDDS);
                         QuotaHelper.CheckNumericQuota(cntx, quotaResults, Quotas.VPS2012_ADDITIONAL_VHD_COUNT, item.HddSize.Length - 1, VirtualizationErrorCodes.QUOTA_EXCEEDED_ADDITIONAL_HDD);
+                    }
+                    else
+                    {
+                        QuotaHelper.CheckNumericQuota(cntx, quotaResults, Quotas.VPS2012_HDD, totalHddSize, VirtualizationErrorCodes.QUOTA_EXCEEDED_HDD);
                     }
                     QuotaHelper.CheckNumericQuota(cntx, quotaResults, Quotas.VPS2012_SNAPSHOTS_NUMBER, item.SnapshotsNumber, VirtualizationErrorCodes.QUOTA_EXCEEDED_SNAPSHOTS);
 
