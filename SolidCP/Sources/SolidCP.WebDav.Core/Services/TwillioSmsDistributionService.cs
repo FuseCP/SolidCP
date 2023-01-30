d6d4bb369a93c1117c06fa37adb0c3ff17697e35
Trevor Robinson <t.robinson@fusecp.com>
2023-01-30 14:26:26 +0100

    Updated Twilio.3.6.29

--- SolidCP/Sources/SolidCP.WebDav.Core/Services/TwillioSmsDistributionService.cs
+++ SolidCP/Sources/SolidCP.WebDav.Core/Services/TwillioSmsDistributionService.cs
@@ -1,31 +1,53 @@
 ﻿using Twilio;
 using SolidCP.WebDav.Core.Config;
 using SolidCP.WebDav.Core.Interfaces.Services;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace SolidCP.WebDav.Core.Services
 {
     public class TwillioSmsDistributionService : ISmsDistributionService
     {
-        private readonly TwilioRestClient _twilioRestClient;
+        //private readonly TwilioRestClient _twilioRestClient;
 
         public TwillioSmsDistributionService()
         {
-            _twilioRestClient = new TwilioRestClient(WebDavAppConfigManager.Instance.TwilioParameters.AccountSid, WebDavAppConfigManager.Instance.TwilioParameters.AuthorizationToken);
+            //_twilioRestClient = new TwilioRestClient(WebDavAppConfigManager.Instance.TwilioParameters.AccountSid, WebDavAppConfigManager.Instance.TwilioParameters.AuthorizationToken);
+               
         }
 
 
         public bool SendMessage(string phoneFrom, string phone, string message)
         {
-            var result = _twilioRestClient.SendSmsMessage(phoneFrom, phone, message);
+            string accountSid = WebDavAppConfigManager.Instance.TwilioParameters.AccountSid;
+            string authToken = WebDavAppConfigManager.Instance.TwilioParameters.AuthorizationToken;
 
-            return string.IsNullOrEmpty(result.Status) == false;
+            TwilioClient.Init(accountSid, authToken);
+
+            var result = MessageResource.Create(
+                body: message,
+                from: new Twilio.Types.PhoneNumber(phoneFrom),
+                to: new Twilio.Types.PhoneNumber(phone)
+            );
+
+            return string.IsNullOrEmpty(result.Status.ToString()) == false;
+
+
         }
 
         public bool SendMessage(string phone, string message)
         {
-            var result = _twilioRestClient.SendSmsMessage(WebDavAppConfigManager.Instance.TwilioParameters.PhoneFrom, phone, message);
+            string accountSid = WebDavAppConfigManager.Instance.TwilioParameters.AccountSid;
+            string authToken = WebDavAppConfigManager.Instance.TwilioParameters.AuthorizationToken;
 
-            return string.IsNullOrEmpty(result.Status) == false;
+            TwilioClient.Init(accountSid, authToken);
+
+            var result = MessageResource.Create(
+                body: message,
+                from: new Twilio.Types.PhoneNumber(WebDavAppConfigManager.Instance.TwilioParameters.PhoneFrom),
+                to: new Twilio.Types.PhoneNumber(phone)
+            );
+
+            return string.IsNullOrEmpty(result.Status.ToString()) == false;
         }
     }
 }
