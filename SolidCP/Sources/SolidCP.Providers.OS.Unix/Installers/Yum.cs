8384ffcb70324d94d2c1748be649f749003a4b2b
Simon Egli <simon.jakob.egli@gmail.com>
2023-11-07 23:37:30 -0600

    fix

---
+++ SolidCP/Sources/SolidCP.Providers.OS.Unix/Installers/Yum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidCP.Providers.OS
+{
+    public class Yum : Installer
+    {
+        public override bool IsInstalled => Shell.Find("yum") != null;
+
+        public override void AddSources(string sources)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Shell InstallAsync(string apps)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
