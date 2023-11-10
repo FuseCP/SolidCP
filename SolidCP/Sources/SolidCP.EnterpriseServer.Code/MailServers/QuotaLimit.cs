d528a8b909becf48409cf6885181084e051e0596
Simon Egli <simon.jakob.egli@gmail.com>
2023-11-09 21:47:06 -0600

    Working on EnterpriseServer running on net6.
    EnterpriseServer.Code needs to be compiled with netstandard2.0. It still has some dependencies on HttpContext that need to be fixed.

--- SolidCP/Sources/SolidCP.EnterpriseServer.Code/MailServers/QuotaLimit.cs
+++ SolidCP/Sources/SolidCP.EnterpriseServer.Code/MailServers/QuotaLimit.cs
@@ -32,13 +32,6 @@
 
 ﻿using System;
 using System.Data;
-using System.Configuration;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
 
 namespace SolidCP.EnterpriseServer.Code.MailServers
 {
--- SolidCP/Sources/SolidCP.EnterpriseServer.Code/Provisioning/IBackupController.cs
+++ SolidCP/Sources/SolidCP.EnterpriseServer.Code/Provisioning/IBackupController.cs
@@ -32,13 +32,6 @@
 
 using System;
 using System.Data;
-using System.Configuration;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 using System.Xml;
 
 using SolidCP.Providers;
