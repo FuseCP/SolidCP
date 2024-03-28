47e2918cc073f89c2d6e2d334ae16763b4b40a82
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-03-28 23:46:19 +0100

    Added HostingServiceProvider methods to Unix

--- SolidCP/Sources/SolidCP.Providers.OS.Unix/Unix.cs
+++ SolidCP/Sources/SolidCP.Providers.OS.Unix/Unix.cs
@@ -364,6 +364,74 @@ namespace SolidCP.Providers.OS
 		{
 			return !OSInfo.IsWindows;
 		}
+		#region HostingServiceProvider methods
+		public override string[] Install()
+		{
+			List<string> messages = new List<string>();
+
+			// create folder if it not exists
+			try
+			{
+				if (!FileUtils.DirectoryExists(UsersHome))
+				{
+					FileUtils.CreateDirectory(UsersHome);
+				}
+			}
+			catch (Exception ex)
+			{
+				messages.Add(String.Format("Folder '{0}' could not be created: {1}",
+					 UsersHome, ex.Message));
+			}
+			return messages.ToArray();
+		}
+
+		public override void DeleteServiceItems(ServiceProviderItem[] items)
+		{
+			foreach (ServiceProviderItem item in items)
+			{
+				try
+				{
+					if (item is HomeFolder)
+						// delete home folder
+						DeleteFile(item.Name);
+				}
+				catch (Exception ex)
+				{
+					Log.WriteError(String.Format("Error deleting '{0}' {1}", item.Name, item.GetType().Name), ex);
+				}
+			}
+		}
+
+		public override ServiceProviderItemDiskSpace[] GetServiceItemsDiskSpace(ServiceProviderItem[] items)
+		{
+			List<ServiceProviderItemDiskSpace> itemsDiskspace = new List<ServiceProviderItemDiskSpace>();
+			foreach (ServiceProviderItem item in items)
+			{
+				if (item is HomeFolder)
+				{
+					try
+					{
+						string path = item.Name;
+
+						Log.WriteStart(String.Format("Calculating '{0}' folder size", path));
+
+						// calculate disk space
+						ServiceProviderItemDiskSpace diskspace = new ServiceProviderItemDiskSpace();
+						diskspace.ItemId = item.Id;
+						diskspace.DiskSpace = FileUtils.CalculateFolderSize(path);
+						itemsDiskspace.Add(diskspace);
+
+						Log.WriteEnd(String.Format("Calculating '{0}' folder size", path));
+					}
+					catch (Exception ex)
+					{
+						Log.WriteError(ex);
+					}
+				}
+			}
+			return itemsDiskspace.ToArray();
+		}
+		#endregion
 
 		protected string LogName(string fullpath)
 		{
