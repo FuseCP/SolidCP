9fd5acc9404930502e51da0c2b8023cc5c6b7a13
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-03-29 07:29:10 +0100

    Improved StackTrace in MessageBox

--- SolidCP/Sources/SolidCP.WebPortal/DesktopModules/SolidCP/UserControls/MessageBox.ascx.cs
+++ SolidCP/Sources/SolidCP.WebPortal/DesktopModules/SolidCP/UserControls/MessageBox.ascx.cs
@@ -41,102 +41,115 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
-
+using System.Text.RegularExpressions;
 using System.Web.Services.Protocols;
+using System.IO;
+using SolidCP.Providers.OS;
 
 namespace SolidCP.Portal
 {
-    public partial class MessageBox : SolidCPControlBase, IMessageBoxControl, INamingContainer
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            //this.Visible = false;
-            if (ViewState["ShowNextTime"] != null)
-            {
-                this.Visible = true;
-                ViewState["ShowNextTime"] = null;
-            }
-        }
+	public partial class MessageBox : SolidCPControlBase, IMessageBoxControl, INamingContainer
+	{
+		const string SolidCPGithubUrl = "https://github.com/FuseCP/SolidCP";
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			//this.Visible = false;
+			if (ViewState["ShowNextTime"] != null)
+			{
+				this.Visible = true;
+				ViewState["ShowNextTime"] = null;
+			}
+		}
 
-        public void RenderMessage(MessageBoxType messageType, string message, string description,
-            Exception ex, params string[] additionalParameters)
-        {
-            this.Visible = true; // show message
+		public void RenderMessage(MessageBoxType messageType, string message, string description,
+			 Exception ex, params string[] additionalParameters)
+		{
+			this.Visible = true; // show message
 
-            // set icon and styles
-            string boxStyle = "MessageBox Green";
-            if (messageType == MessageBoxType.Warning)
-                boxStyle = "MessageBox Yellow";
-            else if (messageType == MessageBoxType.Error)
-                boxStyle = "MessageBox Red";
+			// set icon and styles
+			string boxStyle = "MessageBox Green";
+			if (messageType == MessageBoxType.Warning)
+				boxStyle = "MessageBox Yellow";
+			else if (messageType == MessageBoxType.Error)
+				boxStyle = "MessageBox Red";
 
-            tblMessageBox.Attributes["class"] = boxStyle;
+			tblMessageBox.Attributes["class"] = boxStyle;
 
-            // set texts
-            litMessage.Text = message;
-            litDescription.Text = !String.IsNullOrEmpty(description)
-                ? String.Format("<br/><span class=\"description\">{0}</span>", description) : "";
+			// set texts
+			litMessage.Text = message;
+			litDescription.Text = !String.IsNullOrEmpty(description)
+				 ? String.Format("<br/><span class=\"description\">{0}</span>", description) : "";
 
-            // show exception
-            if (ex != null)
-            {
-                // show error
-                try
-                {
-                    // technical details
-                    litPageUrl.Text = PortalAntiXSS.Encode(Request.Url.ToString());
-                    litLoggedUser.Text = PanelSecurity.LoggedUser.Username;
-                    litSelectedUser.Text = PanelSecurity.SelectedUser.Username;
-                    litPackageName.Text = PanelSecurity.PackageId.ToString();
-                    litStackTrace.Text = ex.ToString().Replace("\n", "<br/>");
+			// show exception
+			if (ex != null)
+			{
+				// show error
+				try
+				{
+					// technical details
+					litPageUrl.Text = PortalAntiXSS.Encode(Request.Url.ToString());
+					litLoggedUser.Text = PanelSecurity.LoggedUser.Username;
+					litSelectedUser.Text = PanelSecurity.SelectedUser.Username;
+					litPackageName.Text = PanelSecurity.PackageId.ToString();
+					ViewState["MessageBox.StackTrace"] = ex.ToString();
+					var stackhtml = ex.ToString();
+					var fileVersion = OSInfo.SolidCPVersion;
+					stackhtml = Regex.Replace(stackhtml, @"(?<=\n\s*at\s+.+\)\s+in\s+)(?:[A-Za-z]:\\|/)[^:]+(?=:line\s+[0-9]+\r?\n)", match =>
+					{
+						var file = match.Value.Replace(Path.DirectorySeparatorChar, '/');
+						file = Regex.Replace(file, @"^.*?(?=/SolidCP/(?:Sources|Lib)/)", "");
+						return $@"<a href=""{SolidCPGithubUrl}/tree/v{fileVersion}{file}"">{file}</a>";
+					}, RegexOptions.Multiline);
+					stackhtml = stackhtml.Replace("\n", "<br/>");
+					litStackTrace.Text = stackhtml;
 
-                    // send form
-                    litSendFrom.Text = PanelSecurity.LoggedUser.Email;
+					// send form
+					litSendFrom.Text = PanelSecurity.LoggedUser.Email;
 
-                    if (!String.IsNullOrEmpty(PortalUtils.FromEmail))
-                        litSendFrom.Text = PortalUtils.FromEmail;
+					if (!String.IsNullOrEmpty(PortalUtils.FromEmail))
+						litSendFrom.Text = PortalUtils.FromEmail;
 
-                    //litSendTo.Text = this.PortalSettings.Email;
-                    litSendTo.Text = PortalUtils.AdminEmail;
-                    litSendCC.Text = PanelSecurity.LoggedUser.Email;
-                    litSendSubject.Text = GetLocalizedString("Text.Subject");
-                }
-                catch { /* skip */ }
-            }
-            else
-            {
-                rowTechnicalDetails.Visible = false;
-            }
-        }
+					//litSendTo.Text = this.PortalSettings.Email;
+					litSendTo.Text = PortalUtils.AdminEmail;
+					litSendCC.Text = PanelSecurity.LoggedUser.Email;
+					litSendSubject.Text = GetLocalizedString("Text.Subject");
+				}
+				catch { /* skip */ }
+			}
+			else
+			{
+				rowTechnicalDetails.Visible = false;
+			}
+		}
 
-        protected void btnSend_Click(object sender, EventArgs e)
-        {
-            EnableViewState = true;
-            ViewState["ShowNextTime"] = true;
+		protected void btnSend_Click(object sender, EventArgs e)
+		{
+			EnableViewState = true;
+			ViewState["ShowNextTime"] = true;
 
-            StringBuilder sb = new StringBuilder();
-            sb.Append("Page URL: ").Append(litPageUrl.Text).Append("\n\n");
-            sb.Append("Logged User: ").Append(litLoggedUser.Text).Append("\n\n");
-            sb.Append("Selected User: ").Append(litSelectedUser.Text).Append("\n\n");
-            sb.Append("Package ID: ").Append(litPackageName.Text).Append("\n\n");
-            sb.Append("Stack Trace: ").Append(litStackTrace.Text.Replace("<br/>", "\n")).Append("\n\n");
-            sb.Append("Personal Comments: ").Append(txtSendComments.Text).Append("\n\n");
+			StringBuilder sb = new StringBuilder();
+			sb.Append("Page URL: ").Append(litPageUrl.Text).Append("\n\n");
+			sb.Append("Logged User: ").Append(litLoggedUser.Text).Append("\n\n");
+			sb.Append("Selected User: ").Append(litSelectedUser.Text).Append("\n\n");
+			sb.Append("Package ID: ").Append(litPackageName.Text).Append("\n\n");
+			sb.Append("Stack Trace: ").Append((string)ViewState["MessageBox.StackTrace"]).Append("\n\n");
+			sb.Append("Personal Comments: ").Append(txtSendComments.Text).Append("\n\n");
 
-            try
-            {
-                btnSend.Visible = false;
-                lblSentMessage.Visible = true;
+			try
+			{
+				btnSend.Visible = false;
+				lblSentMessage.Visible = true;
 
-                // send mail
-                PortalUtils.SendMail(litSendFrom.Text, litSendTo.Text, litSendFrom.Text,
-                    litSendSubject.Text, sb.ToString());
+				// send mail
+				PortalUtils.SendMail(litSendFrom.Text, litSendTo.Text, litSendFrom.Text,
+					 litSendSubject.Text, sb.ToString());
 
-                lblSentMessage.Text = GetLocalizedString("Text.MessageSent");
-            }
-            catch
-            {
-                lblSentMessage.Text = GetLocalizedString("Text.MessageSentError");
-            }
-        }
-    }
+				lblSentMessage.Text = GetLocalizedString("Text.MessageSent");
+			}
+			catch
+			{
+				lblSentMessage.Text = GetLocalizedString("Text.MessageSentError");
+			}
+		}
+	}
 }
