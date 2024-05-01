f41cfc818fc10db7f2106e37033628da79aa4c6f
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-05-01 07:20:22 +0200

    Deleted old POCO generator model

--- SolidCP/Sources/SolidCP.EnterpriseServer.Data/Model/Core/Generator/CreateModel.cs
+++
@@ -1,3 +0,0 @@
-﻿// Licence file C:\Users\simon\Documents\ReversePOCO.txt not found.
-// Please obtain your licence file at www.ReversePOCO.co.uk, and place it in your documents folder shown above.
-// Defaulting to Trial version.
