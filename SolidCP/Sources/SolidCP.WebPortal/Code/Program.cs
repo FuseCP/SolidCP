bdbbb38e5e7286140b5bd102b5237bb0dd467622
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-10-26 23:45:18 +0200

    Added WebFormsForCore support to Portal

---
+++ SolidCP/Sources/SolidCP.WebPortal/Code/Program.cs
@@ -0,0 +1,31 @@
+﻿#if NETCOREAPP
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+		builder.Services.AddRazorPages();
+		builder.Services.AddControllersWithViews();
+
+		var app = builder.Build();
+
+		app.UseStaticFiles();
+
+		//app.UseAuthorization();
+
+		app.UseWebForms();
+
+		app.MapDefaultControllerRoute();
+
+		app.Run();
+
+	}
+}
+#endif
