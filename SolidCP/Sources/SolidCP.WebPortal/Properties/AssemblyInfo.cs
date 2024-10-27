e6e0f344fa1a982951f55734b883cbc70c937abe
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-10-27 19:45:23 +0100

    Changed intermediatepath for Portal

--- SolidCP/Sources/SolidCP.WebPortal/Properties/AssemblyInfo.cs
+++ SolidCP/Sources/SolidCP.WebPortal/Properties/AssemblyInfo.cs
@@ -38,10 +38,10 @@ using System.Web.UI;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
+//[assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
 
 // embedded resources
 [assembly: WebResource("SolidCP.WebPortal.Code.Adapters.empty.gif", "image/gif")]
