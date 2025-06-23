eafdc58101563dae0a7f1a43337af1645cfa08d2
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2025-06-23 12:26:48 +0200

    Moved to AspNetCoreSharedServer 1.0.20

--- SolidCP.Installer/Sources/SolidCP.UniversalInstaller.Core/Installers/OSSpecific/UnixInstaller.cs
+++ SolidCP.Installer/Sources/SolidCP.UniversalInstaller.Core/Installers/OSSpecific/UnixInstaller.cs
@@ -30,7 +30,7 @@ public abstract class UnixInstaller : Installer
 	{
 		Log.WriteStart("Install AspNetCoreSharedServer");
 
-		const string AspNetCoreSharedServerVersion = "1.0.18";
+		const string AspNetCoreSharedServerVersion = "1.0.20";
 
 		if (installedAspNetCoreSharedServer) return;
 		installedAspNetCoreSharedServer = true;
