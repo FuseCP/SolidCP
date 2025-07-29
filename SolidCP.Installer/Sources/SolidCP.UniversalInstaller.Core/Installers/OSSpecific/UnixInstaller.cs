38c5a1c5e4c0ba12521b4dba054393052d5c46d4
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2025-07-29 19:13:11 +0200

    moved to ANCSS 1.1.9

--- SolidCP.Installer/Sources/SolidCP.UniversalInstaller.Core/Installers/OSSpecific/UnixInstaller.cs
+++ SolidCP.Installer/Sources/SolidCP.UniversalInstaller.Core/Installers/OSSpecific/UnixInstaller.cs
@@ -30,7 +30,7 @@ public abstract class UnixInstaller : Installer
 	{
 		Log.WriteStart("Install AspNetCoreSharedServer");
 
-		const string AspNetCoreSharedServerVersion = "1.1.8";
+		const string AspNetCoreSharedServerVersion = "1.1.9";
 
 		if (installedAspNetCoreSharedServer) return;
 		installedAspNetCoreSharedServer = true;
